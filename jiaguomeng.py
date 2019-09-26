ff5a00c98df334b502dd93a274b7dcddbc9cd874
SQRPI <sqr_pi@163.com>
2019-09-26 16:45:11 +0800

    Update jiaguomeng.py
    fix bugs

--- jiaguomeng.py
+++ jiaguomeng.py
@@ -40,28 +40,28 @@ buffs_100 = {
 
 buffs_50 = {'零件厂': ['企鹅机械']}
 
-buffs_com = {'人才公寓': [1.2, 1.5, 1.8],
-             '中式小楼': [1.2, 1.5, 1.8],
-             '空中别墅': [1.2, 1.5, 1.8],
-             '民食斋': [1.2, 1.5, 1.8],
-             '媒体之声': [1.05, 1.1, 1.15],
-             '电厂': [1.2, 1.5, 1.8],
-             '纺织厂':[1.15, 1.3, 1.5],
-             '企鹅机械':[1.1, 1.2, 1.3]}
-buffs_ind = {'人才公寓': [1.38, 1.95, 2.7],
-             '中式小楼': [1.2, 1.5, 1.8],
-             '空中别墅': [1.2, 1.5, 1.8],
-             '民食斋': [1.2, 1.5, 1.8],
-             '媒体之声': [1.05, 1.1, 1.15],
-             '电厂': [1.2, 1.5, 1.8],
-             '企鹅机械':[1.1, 1.2, 1.3]}
-buffs_res = {'人才公寓': [1.2, 1.5, 1.8],
-             '中式小楼': [1.38, 1.95, 2.7],
-             '空中别墅': [1.2, 1.5, 1.8],
-             '民食斋': [1.2, 1.5, 1.8],
-             '媒体之声': [1.05, 1.1, 1.15],
-             '电厂': [1.2, 1.5, 1.8],
-             '企鹅机械':[1.1, 1.2, 1.3]
+buffs_com = {'人才公寓': [.2, .5, .8],
+             '中式小楼': [.2, .5, .8],
+             '空中别墅': [.2, .5, .8],
+             '民食斋': [.2, .5, .8],
+             '媒体之声': [.05, .1, .15],
+             '电厂': [.2, .5, .8],
+             '纺织厂':[.15, .3, .5],
+             '企鹅机械':[.1, .2, .3]}
+buffs_ind = {'人才公寓': [.35, 8, 1.3],
+             '中式小楼': [.2, .5, .8],
+             '空中别墅': [.2, .5, .8],
+             '民食斋': [.2, .5, .8],
+             '媒体之声': [.05, .1, .15],
+             '电厂': [.2, .5, .8],
+             '企鹅机械':[.2, .5, .8]}
+buffs_res = {'人才公寓': [.2, .5, .8],
+             '中式小楼': [.35, 8, 1.3],
+             '空中别墅': [.2, .5, .8],
+             '民食斋': [.2, .5, .8],
+             '媒体之声': [.05, .1, .15],
+             '电厂': [.2, .5, .8],
+             '企鹅机械':[.1, .2, .3]
              }
 #
 #commercial = '木屋 居民楼 钢结构房 平房 小型公寓 人才公寓 花园洋房 中式小楼 空中别墅 复兴公馆'.split()
@@ -70,9 +70,17 @@ buffs_res = {'人才公寓': [1.2, 1.5, 1.8],
 #
 #removelist = '复兴公馆 空中别墅 花园洋房 加油站 商贸中心 人民石油'.split()
 
-commercial = '木屋 居民楼 钢结构房 平房 小型公寓 人才公寓 中式小楼'.split()
-industry   = '便利店 五金店 服装店 菜市场 学校 图书城 民食斋 媒体之声'.split()
-residence  = '木材厂 食品厂 造纸厂 水厂 电厂 钢铁厂 纺织厂 零件厂 企鹅机械'.split()
+#residence = '木屋 居民楼 钢结构房 平房 人才公寓 复兴公馆'.split()
+#commercial   = '便利店 五金店 服装店 菜市场 学校 图书城 民食斋 媒体之声'.split()
+#industry  = '木材厂 食品厂 造纸厂 电厂 钢铁厂 纺织厂 零件厂 企鹅机械'.split()
+#
+#OneStars = '民食斋 人才公寓 零件厂 企鹅机械 复兴公馆 媒体之声'.split()
+#TwoStars = '纺织厂 电厂 平房 学校 图书城 钢铁厂'.split()
+#TriStars = '居民楼 木屋 钢结构房 便利店 木材厂 服装店 五金店 菜市场 造纸厂 食品厂'.split()
+
+residence = '木屋 居民楼 钢结构房 平房 小型公寓 人才公寓 中式小楼'.split()
+commercial = '便利店 五金店 服装店 菜市场 学校 图书城 民食斋 媒体之声'.split()
+industry  = '木材厂 食品厂 造纸厂 水厂 电厂 钢铁厂 纺织厂 零件厂 企鹅机械'.split()
 
 OneStars = '中式小楼 民食斋 人才公寓 小型公寓 图书城 媒体之声 水厂 零件厂 企鹅机械'.split()
 TwoStars = '纺织厂 木屋 电厂 平房 学校 钢铁厂'.split()
@@ -86,40 +94,47 @@ for item in TwoStars:
 for item in TriStars:
     star[item] = 3
 
+startDict = {1:1, 2:2, 3:6, 4:24}
+
+######星级 * 照片 * 政策 * 任务
+
 start = dict()
 for item in commercial:
-    start[item] = 2.5*star[item]
+    start[item] = startDict[star[item]]*3.3*5.1
 for item in industry:
-    start[item] = 1.6*star[item]
+    start[item] = startDict[star[item]]*2.4*2.1
 for item in residence:
-    start[item] = 2.5*star[item]
+    start[item] = startDict[star[item]]*3.3*5.1*1.3
 
-######政策
+#
+#start['纺织厂'] *= 2.5
+#start['木材厂'] *= 2
+#start['食品厂'] *= 2.5
 
-start['纺织厂'] *= 2.5
-start['木材厂'] *= 2
-start['食品厂'] *= 2.5
+start['小型公寓'] *= 2
 
 
 def calculateComb(buildings):
     buildtuple = buildings[0] + buildings[1] + buildings[2]
-    results = [start[x] for x in buildtuple]
+    starts = [start[x] for x in buildtuple]
+    results = [1] * 9
     for item in buildtuple:
         if item in buffs_100:
             for buffed in buffs_100[item]:
                 if buffed in buildtuple:
-                    results[buildtuple.index(buffed)] *= star[item]+1
+                    results[buildtuple.index(buffed)] += star[item]
         if item in buffs_50:
             for buffed in buffs_100[item]:
                 if buffed in buildtuple:
-                    results[buildtuple.index(buffed)] *= star[item]*0.5+1
-        if item in buffs_res:
-            results[0:3] = np.dot(results[0:3], buffs_res[item][star[item]-1])
+                    results[buildtuple.index(buffed)] += star[item]*0.5
         if item in buffs_com:
-            results[3:6] = np.dot(results[3:6], buffs_com[item][star[item]-1])
+            results[0:3] = np.add(results[0:3], buffs_com[item][star[item]-1])
         if item in buffs_ind:
-            results[6:9] = np.dot(results[6:9], buffs_ind[item][star[item]-1])
-    return np.sum(results)
+            results[3:6] = np.add(results[3:6], buffs_ind[item][star[item]-1])
+        if item in buffs_res:
+            results[6:9] = np.add(results[6:9], buffs_res[item][star[item]-1])
+    return (np.sum([v*results[i] for i, v in enumerate(starts)]),
+            [v*results[i]/startDict[star[buildtuple[i]]] for i, v in enumerate(starts)])
 #
 results = PQ()
 #
@@ -140,15 +155,13 @@ for item in tqdm(itertools.product(itertools.combinations(commercial, 3), iterto
 #    if prod > Max:
 #        print('\n', prod, item)
 #        Max = prod
-    results.put(Result(-prod, item))
+    results.put(Result(-prod[0], (item, prod[1])))
     pass
 
 
 cdict = dict()
-for i in range(10):
+for i in range(2):
     cdict[i] = results.get()
     print(-cdict[i].priority, cdict[i].builds)
-
-
 
 

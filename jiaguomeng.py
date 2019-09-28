f7baeabd575950d0bffefc536907e903b29702d0
SQRPI <sqr_pi@163.com>
2019-09-28 18:33:14 +0800

    Update jiaguomeng.py
    fixed some bug

--- jiaguomeng.py
+++ jiaguomeng.py
@@ -11,6 +11,7 @@ import itertools
 from queue import PriorityQueue as PQ
 from scipy.special import comb
 
+Mode = 'Online'
 
 buffs_100 = {
                 '木屋': ['木材厂'],
@@ -26,7 +27,6 @@ buffs_100 = {
                 '学校':  ['图书城'],
                 '图书城': ['学校', '造纸厂'],
                 '商贸中心': ['花园洋房'],
-                '加油站':['人民石油'],
 
                 '木材厂': ['木屋'],
                 '食品厂': ['菜市场'],
@@ -38,40 +38,65 @@ buffs_100 = {
                 '人民石油':['加油站']
             }
 
-buffs_50 = {'零件厂': ['企鹅机械']}
+buffs_50 = {'零件厂': ['企鹅机械'],
+            '加油站':['人民石油']}
 
-buffs_com = {'人才公寓': [.2, .5, .8],
-             '中式小楼': [.2, .5, .8],
-             '空中别墅': [.2, .5, .8],
-             '民食斋': [.2, .5, .8],
-             '媒体之声': [.05, .1, .15],
-             '电厂': [.2, .5, .8],
-             '纺织厂':[.15, .3, .5],
-             '企鹅机械':[.1, .2, .3]}
-buffs_ind = {'人才公寓': [.35, 8, 1.3],
-             '中式小楼': [.2, .5, .8],
-             '空中别墅': [.2, .5, .8],
-             '民食斋': [.2, .5, .8],
-             '媒体之声': [.05, .1, .15],
-             '电厂': [.2, .5, .8],
-             '企鹅机械':[.2, .5, .8]}
-buffs_res = {'人才公寓': [.2, .5, .8],
-             '中式小楼': [.35, 8, 1.3],
-             '空中别墅': [.2, .5, .8],
-             '民食斋': [.2, .5, .8],
-             '媒体之声': [.05, .1, .15],
-             '电厂': [.2, .5, .8],
-             '企鹅机械':[.1, .2, .3]
+bufflist_258 = [.2, .5, .8, 1.1, 1.4]
+bufflist_246 = [.2, .4, .6, .8, 1]
+bufflist_015 = [.2, .4, .6, .8, 1]*0.75
+bufflist_010 = [.2, .4, .6, .8, 1]*0.5
+bufflist_005 = [.2, .4, .6, .8, 1]*0.25
+bufflist_035 = [.2, .4, .6, .8, 1]*0.25*7
+
+buffs_com = {
+             '媒体之声': bufflist_005,
+             '企鹅机械': bufflist_015,
+             '民食斋': bufflist_246,
+             '纺织厂': bufflist_015,
+             '人才公寓': bufflist_246,
+             '中式小楼': bufflist_246,
+             '空中别墅': bufflist_258,
+             '电厂': bufflist_258
+             }
+buffs_ind = {
+             '媒体之声': bufflist_005,
+             '钢铁厂': bufflist_015,
+             '中式小楼': bufflist_246,
+             '民食斋': bufflist_246,
+             '空中别墅': bufflist_258,
+             '电厂': bufflist_258,
+             '企鹅机械': bufflist_258,
+             '人才公寓': bufflist_035
+             }
+buffs_res = {
+             '媒体之声': bufflist_005,
+             '企鹅机械':bufflist_010,
+             '民食斋': bufflist_246,
+             '人才公寓': bufflist_246,
+             '平房': bufflist_246,
+             '空中别墅': bufflist_258,
+             '电厂': bufflist_258,
+             '中式小楼': bufflist_035
              }
 #
+class UndefinedError(Exception): pass
 
-residence = '木屋 居民楼 钢结构房 平房 小型公寓 人才公寓 中式小楼'.split()
-commercial = '便利店 五金店 服装店 菜市场 学校 图书城 民食斋 媒体之声'.split()
-industry  = '木材厂 食品厂 造纸厂 水厂 电厂 钢铁厂 纺织厂 零件厂 企鹅机械'.split()
+if Mode == 'Online':
+    residence = '木屋 居民楼 钢结构房 平房 小型公寓 人才公寓 中式小楼'.split()
+    commercial = '便利店 服装店 五金店 菜市场 学校 图书城 民食斋 媒体之声'.split()
+    industry  = '木材厂 食品厂 造纸厂 水厂 电厂 钢铁厂 纺织厂 零件厂 企鹅机械'.split()
+elif Mode == 'Offline':
+# TODO:
+    raise UndefinedError('离线收益等我有空再写，真的用得到吗')
+    residence = '木屋 居民楼 钢结构房 平房 小型公寓 人才公寓 中式小楼'.split()
+    commercial = '便利店 服装店 五金店 菜市场 学校 图书城 民食斋 媒体之声'.split()
+    industry  = '木材厂 食品厂 造纸厂 水厂 电厂 钢铁厂 纺织厂 零件厂 企鹅机械'.split()
 
-OneStars = '中式小楼 民食斋 人才公寓 小型公寓 图书城 媒体之声 零件厂 企鹅机械'.split()
-TwoStars = '纺织厂 木屋 电厂 平房 学校 钢铁厂 水厂'.split()
-TriStars = '菜市场 居民楼 便利店 服装店 食品厂 木材厂 钢结构房 五金店 造纸厂'.split()
+OneStars = '媒体之声 企鹅机械'.split()
+TwoStars = '民食斋 人才公寓 零件厂 图书城 中式小楼'.split()
+TriStars = '纺织厂 钢铁厂'.split()
+QuaStars = '便利店 学校 服装店 小型公寓 水厂 菜市场 木屋 居民楼 电厂 五金店 食品厂 平房 木材厂'.split()
+PenStars = '造纸厂 钢结构房'.split()
 
 star = dict()
 for item in OneStars:
@@ -80,31 +105,50 @@ for item in TwoStars:
     star[item] = 2
 for item in TriStars:
     star[item] = 3
+for item in QuaStars:
+    star[item] = 4
+for item in PenStars:
+    star[item] = 5
 
-startDict = {1:1, 2:2, 3:6, 4:24}
-
+startDict = {1:1, 2:2, 3:6, 4:24, 5:120}
 ######星级 * 照片 * 政策 * 任务
 '''
- 我这里的照片加成是全体40%+在线40%+住宅150%+商业150%+工业60%
- 所以住宅和商业建筑的照片加成系数为 1+0.4+0.4+1.5=3.3
- 工业建筑的照片加成系数为 1+0.4+0.4+0.6=2.4
-
- 我的政策加成为 全体100% + 商业300% + 住宅300% + 工业150%
- 加上家国之光的10%加成
- 政策加成系数为 商业住宅 1+1+3+.1 = 5.1
-             工业     1+1+1.5+.1 = 3.6
-
- 最后在住宅上乘上和谐家园的任务加成 1.3
+    在这里填写你的政策和照片加成
 '''
-# TODO: 自动计算
+
+Policy = {
+            'Global':  1+2,
+            'Online':  1.5,
+            'Offline': 1,
+            'Residence': 3,
+            'Commercial': 3,
+            'Industry': 6,
+            'JiaGuoZhiGuang': 0.3
+        }
+
+Photos = {
+            'Global':  0.6,
+            'Online':  0.6,
+            'Offline': 0.9,
+            'Residence': 1.8,
+            'Commercial': 1.8,
+            'Industry': 0.6,
+        }
+
 
 start = dict()
-for item in commercial:#商业
-    start[item] = startDict[star[item]]*3.3*5.1
-for item in industry:#工业
-    start[item] = startDict[star[item]]*2.4*3.6
 for item in residence:#住宅
-    start[item] = startDict[star[item]]*3.3*5.1*1.3
+    start[item] = startDict[star[item]]*\
+        (1+Policy['Global']+Policy['Online']+Policy['Residence']+Policy['JiaGuoZhiGuang'])*\
+        (1+Photos['Global']+Photos['Online']+Photos['Residence'])
+for item in commercial:#商业
+    start[item] = startDict[star[item]]*\
+        (1+Policy['Global']+Policy['Online']+Policy['Commercial']+Policy['JiaGuoZhiGuang'])*\
+        (1+Photos['Global']+Photos['Online']+Photos['Commercial'])
+for item in industry:#工业
+    start[item] = startDict[star[item]]*\
+        (1+Policy['Global']+Policy['Online']+Policy['Industry']+Policy['JiaGuoZhiGuang'])*\
+        (1+Photos['Global']+Photos['Online']+Photos['Industry'])
 
 
 #收益调整
@@ -112,11 +156,23 @@ start['平房'] *= 1.1
 start['民食斋'] *= 1.52
 
 #任务加成调整
+#start['小型公寓'] *= 2
+#start['人才公寓'] *= 3
+#start['居民楼'] *= 2
+#start['木屋'] *= 2
+
 #start['纺织厂'] *= 2.5
 #start['木材厂'] *= 2
-#start['食品厂'] *= 2.5
+#start['食品厂'] *= 2
+#start['菜市场'] *= 2
 
-start['小型公寓'] *= 2
+#start['商贸中心'] *= 2
+start['服装店'] *= 2
+start['造纸厂'] *= 2
+
+#start['民食斋'] *= 2
+start['钢铁厂'] *= 3
+#start['木材厂'] *= 2
 
 
 def calculateComb(buildings):
@@ -129,7 +185,7 @@ def calculateComb(buildings):
                 if buffed in buildtuple:
                     results[buildtuple.index(buffed)] += star[item]
         if item in buffs_50:
-            for buffed in buffs_100[item]:
+            for buffed in buffs_50[item]:
                 if buffed in buildtuple:
                     results[buildtuple.index(buffed)] += star[item]*0.5
         if item in buffs_com:
@@ -138,6 +194,7 @@ def calculateComb(buildings):
             results[3:6] = np.add(results[3:6], buffs_ind[item][star[item]-1])
         if item in buffs_res:
             results[6:9] = np.add(results[6:9], buffs_res[item][star[item]-1])
+#        print(item, results)
     return (np.sum([v*results[i] for i, v in enumerate(starts)]),
             [v*results[i]/startDict[star[buildtuple[i]]] for i, v in enumerate(starts)])
 #
@@ -171,12 +228,17 @@ cdict = dict()
 print('==============')
 Rec = results.get()
 print('最优策略：', Rec.builds[0])
+print('总加成倍率', np.round(sum([x*startDict[star[Rec.builds[0][i//3][i%3]]] for i, x in enumerate(Rec.builds[1])]), 2))
 print('各建筑加成倍率', np.round(Rec.builds[1], 2))
-print('升级优先级', np.round([x*star[Rec.builds[0][i//3][i%3]] for i, x in enumerate(Rec.builds[1])], 2))
-print('==============')
-Rec = results.get()
-print('次优策略：', Rec.builds[0])
-print('各建筑加成倍率', np.round(Rec.builds[1], 2))
-print('升级优先级', np.round([x*star[Rec.builds[0][i//3][i%3]] for i, x in enumerate(Rec.builds[1])], 2))
+print('升级优先级', np.round([x*startDict[star[Rec.builds[0][i//3][i%3]]] for i, x in enumerate(Rec.builds[1])], 2))
+
+def getNext():
+    print('==============')
+    Rec = results.get()
+    print('次优策略：', Rec.builds[0])
+    print('总加成倍率', np.round(sum([x*startDict[star[Rec.builds[0][i//3][i%3]]] for i, x in enumerate(Rec.builds[1])]), 2))
+    print('各建筑加成倍率', np.round(Rec.builds[1], 2))
+    print('升级优先级', np.round([x*startDict[star[Rec.builds[0][i//3][i%3]]] for i, x in enumerate(Rec.builds[1])], 2))
 
 
+getNext()
